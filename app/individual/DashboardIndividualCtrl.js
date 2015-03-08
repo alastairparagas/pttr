45edab4359b46ab7899cc1f4bcaaeff825d59215
Xavier <xthom001@gmail.com>
2015-03-08 16:35:09 -0400

    dummy data

--- app/individual/DashboardIndividualCtrl.js
+++ app/individual/DashboardIndividualCtrl.js
@@ -1,65 +1,110 @@
-(function (window) {
+(function(window) {
     'use strict';
-    
+
     var angular = window.angular;
 
-    angular.module('pttr.individual').controller('DashboardIndividualCtrl', ['$scope', '$http', function ($scope,$http) {
-        $http.get('js/animalinfo.json').success(function(data){
-        		$scope.animals = data;
+    angular.module('pttr.individual').controller('DashboardIndividualCtrl', ['$scope', '$http', function($scope, $http) {
+        $http.get('js/animalinfo.json').success(function(data) {
+            // $scope.animals = data;
         });
         $scope.liked = false;
-        $scope.animals = [
-        	{
-        		name: "monkey",
-        		type: "beast",
-        		description: "Donkey kong like animal"
-        	},
-        	{
-        		name: "monkey",
-        		type: "beast",
-        		description: "Donkey kong like animal"
-        	},
-        	{
-        		name: "monkey",
-        		type: "beast",
-        		description: "Donkey kong like animal"
-        	},
-        	{
-        		name: "monkey",
-        		type: "beast",
-        		description: "Donkey kong like animal"
-        	},
-        	{
-        		name: "monkey",
-        		type: "beast",
-        		description: "Donkey kong like animal"
-        	},
-        	{
-        		name: "monkey",
-        		type: "beast",
-        		description: "Donkey kong like animal"
-        	},
-        	{
-        		name: "monkey",
-        		type: "beast",
-        		description: "Donkey kong like animal"
-        	},
-        	{
-        		name: "monkey",
-        		type: "beast",
-        		description: "Donkey kong like animal"
-        	},
-        	{
-        		name: "monkey",
-        		type: "beast",
-        		description: "Donkey kong like animal"
-        	}
-        ]
-
-       
-        // var viewAnimal = function(name,type){
+        $scope.animals = [{
+            name: "Great Dane",
+            type: "dog",
+            description: "Found abandoned in an old house"
+        }, {
+            name: "Beagle",
+            type: "dog",
+            description: "lost puppy found on the street"
+        }, {
+            name: "Beagle",
+            type: "dog",
+            description: "rescued from an abandoned home"
+        }, {
+            name: "Tabby Cat",
+            type: "cat",
+            description: "found roaming the streets"
+        }, {
+            name: "Chartreux",
+            type: "cat",
+            description: "rescued from an abusive home"
+        }, {
+            name: "Beagle",
+            type: "dog",
+            description: "rescued from an abandoned home"
+        }, {
+            name: "Tabby Cat",
+            type: "cat",
+            description: "found roaming the streets"
+        }, {
+            name: "Chartreux",
+            type: "cat",
+            description: "rescued from an abusive home"
+        }, {
+            name: "Beagle",
+            type: "dog",
+            description: "rescued from an abandoned home"
+        }, {
+            name: "Tabby Cat",
+            type: "cat",
+            description: "found roaming the streets"
+        }, {
+            name: "Chartreux",
+            type: "cat",
+            description: "rescued from an abusive home"
+        }, {
+            name: "Beagle",
+            type: "dog",
+            description: "rescued from an abandoned home"
+        }, {
+            name: "Tabby Cat",
+            type: "cat",
+            description: "found roaming the streets"
+        }, {
+            name: "Chartreux",
+            type: "cat",
+            description: "rescued from an abusive home"
+        }, {
+            name: "Beagle",
+            type: "dog",
+            description: "rescued from an abandoned home"
+        }, {
+            name: "Tabby Cat",
+            type: "cat",
+            description: "found roaming the streets"
+        }, {
+            name: "Chartreux",
+            type: "cat",
+            description: "rescued from an abusive home"
+        }, {
+            name: "Beagle",
+            type: "dog",
+            description: "rescued from an abandoned home"
+        }, {
+            name: "Tabby Cat",
+            type: "cat",
+            description: "found roaming the streets"
+        }, {
+            name: "Chartreux",
+            type: "cat",
+            description: "rescued from an abusive home"
+        }, {
+            name: "Beagle",
+            type: "dog",
+            description: "rescued from an abandoned home"
+        }, {
+            name: "Tabby Cat",
+            type: "cat",
+            description: "found roaming the streets"
+        }, {
+            name: "Chartreux",
+            type: "cat",
+            description: "rescued from an abusive home"
+        }]
+        
+            // var viewAnimal = function(name,type){
 
         // };
     }]);
-    
+
 }(window));
--- app/shelter/DashboardShelterCtrl.js
+++ app/shelter/DashboardShelterCtrl.js
@@ -4,8 +4,153 @@
     var angular = window.angular;
     
     angular.module('pttr.shelter').controller('DashboardShelterCtrl', ['$scope', function ($scope) {
-        
-        
+        $scope.animals = [
+        	{
+        		name: "Great Dane",
+        		type: "dog",
+        		description: "Found abandoned in an old house"
+        	},
+        	{
+        		name: "Beagle",
+        		type: "dog",
+        		description: "lost puppy found on the street"
+        	},
+        	{
+        		name: "Beagle",
+        		type: "dog",
+        		description: "rescued from an abandoned home"
+        	},
+        	{
+        		name: "Tabby Cat",
+        		type: "cat",
+        		description: "found roaming the streets"
+        	},
+        	{
+        		name: "Chartreux",
+        		type: "cat",
+        		description: "rescued from an abusive home"
+        	} ,
+        	{
+        		name: "Beagle",
+        		type: "dog",
+        		description: "rescued from an abandoned home"
+        	},
+        	{
+        		name: "Tabby Cat",
+        		type: "cat",
+        		description: "found roaming the streets"
+        	},
+        	{
+        		name: "Chartreux",
+        		type: "cat",
+        		description: "rescued from an abusive home"
+        	} ,
+        	{
+        		name: "Beagle",
+        		type: "dog",
+        		description: "rescued from an abandoned home"
+        	},
+        	{
+        		name: "Tabby Cat",
+        		type: "cat",
+        		description: "found roaming the streets"
+        	},
+        	{
+        		name: "Chartreux",
+        		type: "cat",
+        		description: "rescued from an abusive home"
+        	} ,
+        	{
+        		name: "Beagle",
+        		type: "dog",
+        		description: "rescued from an abandoned home"
+        	},
+        	{
+        		name: "Tabby Cat",
+        		type: "cat",
+        		description: "found roaming the streets"
+        	},
+        	{
+        		name: "Chartreux",
+        		type: "cat",
+        		description: "rescued from an abusive home"
+        	} ,
+        	{
+        		name: "Beagle",
+        		type: "dog",
+        		description: "rescued from an abandoned home"
+        	},
+        	{
+        		name: "Tabby Cat",
+        		type: "cat",
+        		description: "found roaming the streets"
+        	},
+        	{
+        		name: "Chartreux",
+        		type: "cat",
+        		description: "rescued from an abusive home"
+        	} ,
+        	{
+        		name: "Beagle",
+        		type: "dog",
+        		description: "rescued from an abandoned home"
+        	},
+        	{
+        		name: "Tabby Cat",
+        		type: "cat",
+        		description: "found roaming the streets"
+        	},
+        	{
+        		name: "Chartreux",
+        		type: "cat",
+        		description: "rescued from an abusive home"
+        	} ,
+        	{
+        		name: "Beagle",
+        		type: "dog",
+        		description: "rescued from an abandoned home"
+        	},
+        	{
+        		name: "Tabby Cat",
+        		type: "cat",
+        		description: "found roaming the streets"
+        	},
+        	{
+        		name: "Chartreux",
+        		type: "cat",
+        		description: "rescued from an abusive home"
+        	} 
+        ] 
+        $scope.pledgeusers = [
+                {
+                    username: "Sandra Walice",
+                    pledgetype: "cash",
+                    pledgeamount: "$40"
+                }, {
+                    username: "James brown",
+                    pledgetype: "cash",
+                    pledgeamount: "$40"
+                }, {
+                    username: "SomeOrg ",
+                    pledgetype: "sponsor",
+                    pledgeamount: "$10,000"
+                }
+            ]
+        $scope.adopterusers = [
+                {
+                    username: "Sandra Walice",
+                    animalName: "beagle",
+                    animalType: "dog"
+                }, {
+                    username: "James brown",
+                    animalName: "Akita",
+                    animalType: "dog"
+                }, {
+                    username: "Lisa smith",
+                    animalName: "ragdoll",
+                    animalType: "cat"
+                }
+            ]
         
     }]);
     
